7cffd27780fbc7c8f7f2f4814266a409b5b4f8ce
Cass <dumpitca@outlook.com>
2024-05-08 18:54:21 +0800

    Created home page

---
+++ scripts/tabs.js
@@ -0,0 +1,5 @@
+handleConditions = () => {
+  const condition = document.querySelector("#conditionsTab");
+  const conditionText = document.querySelector("#conditionText");
+  console.log("clicked");
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,10 @@ module.exports = {
       colors: {
         background: "rgba(240, 240,  242, <alpha-value>)",
         silver: "rgba(251, 251,  251, <alpha-value>)",
-        blue: "rgba(187, 222, 255, <alpha-value>)",
+        "light-blue": "rgba(187, 222, 255, <alpha-value>)",
+        blue: {
+          900: "rgba(15, 81, 140, <alpha-value>)",
+        },
         dark: "rgba(0, 36, 69, <alpha-value>)",
       },
     },
