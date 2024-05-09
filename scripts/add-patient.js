52ece27a2db620dc586a0b566b47865f22b353d6
Cass <dumpitca@outlook.com>
2024-05-10 00:48:14 +0800

    Created basic form functionality and display data

--- scripts/add-patient.js
+++ scripts/add-patient.js
@@ -2,10 +2,39 @@ const form = document
   .querySelector("#addPatient")
   .addEventListener("submit", (e) => {
     e.preventDefault();
+    const firstName = document.querySelector("#firstName").value;
+    const middleName = document.querySelector("#middleName").value;
+    const lastName = document.querySelector("#lastName").value;
+    const name = firstName.concat(" ", middleName, " ", lastName);
+
+    const sex = document.querySelector("#sex").value;
+    const birthday = document.querySelector("#birthday").value;
+    const age = document.querySelector("#age").value;
+    const address = document.querySelector("#address").value;
+    const civilStatus = document.querySelector("#civilStatus").value;
+    const religion = document.querySelector("#religion").value;
+    const nationality = document.querySelector("#nationality").value;
+    const weight = document.querySelector("#weight").value;
+    const height = document.querySelector("#height").value;
+    const scheduleDate = document.querySelector("#scheduleDate").value;
+    const diagnosis = document.querySelector("#diagnosis").value;
+    const doctor = document.querySelector("#doctor").value;
 
     // get form data
     let formData = {
-      name: document.querySelector("#firstName").value,
+      name: name,
+      sex: sex,
+      birth: birthday,
+      age: age,
+      address: address,
+      civil: civilStatus,
+      religion: religion,
+      nationality: nationality,
+      weight: weight,
+      height: height,
+      scheduleDate: scheduleDate,
+      diagnosis: diagnosis,
+      doctor: doctor,
     };
 
     // convert formData to json
@@ -15,5 +44,5 @@ const form = document
     localStorage.setItem("formData", json);
 
     //Redirect to display page
-    window.location.href = "/pages/patients/patients.html";
+    window.location.href = "/pages/patients.html";
   });
--- scripts/get-patient.js
+++ scripts/get-patient.js
@@ -1,11 +1,35 @@
 let json = localStorage.getItem("formData");
 
 // parse json data to javascript object
-
 let formData = JSON.parse(json);
 
 // create html to be rendered
 let patientName = `${formData.name}`;
+let sex = `${formData.sex}`;
+let birth = `${formData.birth}`;
+let age = `${formData.age}`;
+let address = `${formData.address}`;
+let civilStatus = `${formData.civil}`;
+let religion = `${formData.religion}`;
+let nationality = `${formData.nationality}`;
+let weight = `${formData.weight}`;
+let height = `${formData.height}`;
+let schedule = new Date(formData.scheduleDate).toLocaleDateString();
+
+let diagnosis = `${formData.diagnosis}`;
+let doctor = `${formData.doctor}`;
 
 // render data in container
 document.querySelector("#patientName").innerHTML = patientName;
+document.querySelector("#sex").innerHTML = sex;
+document.querySelector("#birthday").innerHTML = birth;
+document.querySelector("#age").innerHTML = age;
+document.querySelector("#address").innerHTML = address;
+document.querySelector("#civilStatus").innerHTML = civilStatus;
+document.querySelector("#religion").innerHTML = religion;
+document.querySelector("#nationality").innerHTML = nationality;
+document.querySelector("#weight").innerHTML = weight.concat("kg");
+document.querySelector("#height").innerHTML = height.concat("cm");
+document.querySelector("#schedule").innerHTML = schedule;
+// document.querySelector("#diagnosis").innerHTML = diagnosis;
+document.querySelector("#doctor").innerHTML = doctor;
